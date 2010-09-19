cf76f96d2b11aaa47cfd914459d91e5497e13bfd
Thiago Coraini <tcoraini@gmail.com>
2010-09-19 19:55:43 -0300

    Modificações nos testes, testando seriação de mailbox

--- my-tests/src/main/scala/GeneralTests.scala
+++ my-tests/src/main/scala/GeneralTests.scala
@@ -2,10 +2,14 @@ package tests
 
 import se.scalablesolutions.akka.actor.Actor
 import se.scalablesolutions.akka.actor.Actor._
+import se.scalablesolutions.akka.actor.HotSwap
 
 import se.scalablesolutions.akka.actor.ActorSerialization._
 import se.scalablesolutions.akka.actor.Format
 import se.scalablesolutions.akka.actor.StatelessActorFormat
+import se.scalablesolutions.akka.actor.SerializerBasedActorFormat
+
+import se.scalablesolutions.akka.serialization.Serializer
 
 import se.scalablesolutions.akka.config.Config.config
 
@@ -22,33 +26,12 @@ class ExampleActor extends Actor {
    }
 }
 
-class MyActor extends Actor {
-   var count = 0
-
-      def receive = {
-         case "hello" =>
-            count = count + 1
-            self.reply("world " + count)
-      }
-}
-
-/*object BinaryFormatMyActor {
-   implicit object MyActorFormat extends Format[MyActor] {
-      def fromBinary(bytes: Array[Byte], act: MyActor) = {
-         val p = Serializer.Protobuf.fromBinary(bytes, Some(classOf[ProtobufProtocol.Counter])).asInstanceOf[ProtobufProtocol.Counter]
-            act.count = p.getCount
-            act
-      }
-      def toBinary(ac: MyActor) =
-         ProtobufProtocol.Counter.newBuilder.setCount(ac.count).build.toByteArray
-   }
-}*/
-
 case class Wait(seconds: Int)
 case object Ack
+case class Garbage(what: String)
 
 class MyStatelessActor extends Actor {
-   self.makeRemote("localhost", 9999)
+   //self.makeRemote("localhost", 9999)
 
    def receive = {
       case Wait(x) => 
@@ -65,8 +48,26 @@ object BinaryFormatMyStatelessActor {
    implicit object MyStatelessActorFormat extends StatelessActorFormat[MyStatelessActor]
 }
 
+@serializable class MyJavaSerializableActor extends Actor {
+  var count = 0
+ 
+  def receive = {
+    case "hello" =>
+      count = count + 1
+      Thread.sleep(1000)
+      println("[" + this + "] Received 'hello'. Current mailbox size: " + self.mailboxSize)
+  }
+}
+
+object BinaryFormatMyJavaSerializableActor {
+  implicit object MyJavaSerializableActorFormat extends SerializerBasedActorFormat[HotSwapActor] {
+    val serializer = Serializer.Java
+  }
+}
+
 object GeneralTests {
    import BinaryFormatMyStatelessActor._
+   //import BinaryFormatMyJavaSerializableActor._
 
    def main(args: Array[String]) {
 
@@ -74,22 +75,34 @@ object GeneralTests {
 
    def execute() {
       val actor1 = actorOf[MyStatelessActor].start
+      //val actor1 = actorOf[HotSwapActor].start
 
-      (actor1 ! Ack)
-      //(actor1 ! Wait(5))
-      //(actor1 ! Ack)
-      //(actor1 ! Ack)
+      actor1 ! Ack
+      //actor1 ! Garbage("Lixo 1")
+      //actor1 ! Garbage("Lixo 2")
+      //actor1 ! Garbage("Lixo 3")
 
+      (actor1 ! Wait(5))
+      actor1 ! Ack
+      actor1 ! Ack
+      actor1 ! Ack
+      actor1 ! Ack
+
+      println("Tamanho do mailbox do ator 1: " + actor1.mailboxSize)
       //val reply1: String = (actor1 !! Ack).getOrElse("_").asInstanceOf[String]
       //println("First reply: " + reply1)
-
+      //Thread.sleep(2000)
       // Fazendo a seriacao
       println("Start of serialization")
       val bytes = toBinary(actor1)
       val actor2 = fromBinary(bytes)
       println("End of serialization")
 
-      actor2 ! Ack
+      println("Tamanho do mailbox do ator 2: " + actor2.mailboxSize)
+
+      //actor2 ! Ack
+      //actor2 ! HotSwap(Some({case any => println("## Mensagem recebida (ator 2): " + any)}))
+      //actor1 ! HotSwap(Some({case any => println("## Mensagem recebida (ator 1): " + any)}))
 
       // Novo ator, deve ser igual ao anterior
       //actor2.start
